f5fa95e3d16a7d869fef140ad5930dea1c6b9374
madderlake <mary@maryahayne.com>
2020-07-14 16:43:53 -0700

    updating templates with acf

--- client/src/components/templates/Default/index.js
+++ client/src/components/templates/Default/index.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ContentBlock from '../../utilities/ContentBlock';
 import { Container } from 'reactstrap';
-//import Section from '../../layout/Section/Section';
 import './index.css';
 
 class Default extends Component {
   render() {
     if (this.props.data) {
       let data = this.props.data;
-      console.log(this.props.data);
+      //console.table(crData);
 
       return (
-        <section>
-          <article className={`${this.props.slug} default-template`}>
-            <Container>
-              <h1>{data.title.rendered}</h1>
-              <ContentBlock content={data.content.rendered} />
-            </Container>
-          </article>
-        </section>
+        <Fragment>
+          <section>
+            <article className={`${this.props.slug} default-template`}>
+              <Container>
+                <h1>{data.title.rendered}</h1>
+                <ContentBlock content={data.content.rendered} />
+              </Container>
+            </article>
+          </section>
+          <section></section>
+        </Fragment>
       );
     }
 
